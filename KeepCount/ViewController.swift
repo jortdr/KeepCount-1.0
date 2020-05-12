f8224c591a04f453ca27e0278a7007c0ff2917f4
Jort Driegen <jort@jortdriegen.nl>
2020-05-12 23:14:09 +0200

    Version 1.0 complete!!
    - Added counter
    - Added + and - buttons

--- KeepCount/ViewController.swift
+++ KeepCount/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var CountLabel: UILabel!
+    
+    @IBOutlet weak var AddOneButton: UIButton!
+    @IBOutlet weak var RemoveOneButton: UIButton!
+    
+    
+    
+    
+    var count:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func AddOnePressed(_ sender: Any) {
+        count += 1
+        
+        CountLabel.text = String(count)
+    }
+    @IBAction func RemOnePressed(_ sender: Any) {
+        if count != 0 {
+            count -= 1
+            
+            CountLabel.text = String(count)
+        }
+    }
 }
 
