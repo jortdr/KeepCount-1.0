d04bfeddef3407ac5d46850af59880fae2d03ab4
Jort Driegen <jort@jortdriegen.nl>
2020-05-13 14:59:35 +0200

    Version 1.0.0.1
    - Added proper constraints
    - Added background colors for various stepper values
    - Added reset button
    
    - Fixed constraint issue
    
    -Removed label

--- KeepCount/ViewController.swift
+++ KeepCount/ViewController.swift
@@ -12,30 +12,37 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var CountLabel: UILabel!
-    
-    @IBOutlet weak var AddOneButton: UIButton!
-    @IBOutlet weak var RemoveOneButton: UIButton!
-    
-    
-    
-    
     var count:Int = 0
+    
+    @IBOutlet var background: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func AddOnePressed(_ sender: Any) {
-        count += 1
-        
-        CountLabel.text = String(count)
-    }
-    @IBAction func RemOnePressed(_ sender: Any) {
-        if count != 0 {
-            count -= 1
+    @IBAction func stepper(_ sender: UIStepper) {
+        if count == 30 {
             
-            CountLabel.text = String(count)
+        }
+        
+        count = Int(sender.value)
+        CountLabel.text = String(count)
+        
+        ChangeBGColor()
+          
+    }
+    
+    func ChangeBGColor() -> Void {
+        if count >= 30 {
+            background.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        } else if count >= 20 && count <= 29 {
+            background.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        } else if count >= 10 && count <= 19 {
+            background.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        } else if count <= 9 {
+            background.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
         }
     }
 }
