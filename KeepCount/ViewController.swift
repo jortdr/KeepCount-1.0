955258d8ea82fff0ca3571bb4f09b3354414f7af
Jort Driegen <jort@jortdriegen.nl>
2020-05-13 16:01:25 +0200

    1.0.0.2
    - Fixed reset button
    - Fixed amount limit

--- KeepCount/ViewController.swift
+++ KeepCount/ViewController.swift
@@ -15,26 +15,45 @@ class ViewController: UIViewController {
     var count:Int = 0
     
     @IBOutlet var background: UIView!
+    @IBOutlet weak var ResetButton: UIButton!
+    @IBOutlet weak var stepper: UIStepper!
+
+    
+    let tooMany = UIAlertController(title: "Actie niet toegestaan", message: "Je zit nu op het huidige maximumaantal van 30 personen.", preferredStyle: .alert)
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tooMany.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        
+        
     }
 
     @IBAction func stepper(_ sender: UIStepper) {
-        if count == 30 {
-            
-        }
+        if stepper.value > 30 {
+            self.present(tooMany, animated: true)
+            stepper.value -= 1
+        } else {
         
         count = Int(sender.value)
-        CountLabel.text = String(count)
-        
         ChangeBGColor()
-          
+            
+            
+        }
+    }
+    
+    @IBAction func ResetPress(_ sender: Any) {
+        count = 0
+        stepper.value = 0
+        CountLabel.text = String(count)
+        ChangeBGColor()
+        
     }
     
     func ChangeBGColor() -> Void {
+        CountLabel.text = String(count)
+        
         if count >= 30 {
             background.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
         } else if count >= 20 && count <= 29 {
